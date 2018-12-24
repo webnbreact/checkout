6c8f7c1227254891180739ae157734fe2b50c3b5
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-23 22:45:54 -0800

    Add basic html structure for Header component

--- client/components/Book.jsx
+++ client/components/Book.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Dates from './Dates.jsx';
+import Guests from './Guests.jsx';
 
 const Book = () => (
   <form action="">
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Header = () => (
   <div>
-    <span>$XX</span>
-    <span> per night</span>
+    <div>
+      <span>$XX</span>
+      <span> per night</span>
+    </div>
+    <div>
+      <button type="button">
+        <span>
+          <span>[stars images]</span>
+          <span>[reviews count]</span>
+        </span>
+      </button>
+    </div>
+    <div>
+      [divider line]
+    </div>
   </div>
 );
 
