3fa721e847936b68bbe28a78a0745700a5f5cd85
Johnson Kwok <johnsonkwok1@gmail.com>
2018-12-25 03:17:01 -0800

    Request room info when App component initially mounts
    * Refactor snapshot test for App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,10 +5,38 @@ import Book from './Book.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      nightlyPrice: '',
+      pastWeekViews: '',
+      cleaningFee: '',
+      serviceFee: '',
+      lastUpdated: '',
+      reviewCount: '',
+      avgRating: '',
+    };
+  }
+
+  componentDidMount() {
+    const roomId = window.location.pathname.substring(7, 10);
+    fetch(`/rooms/${roomId}/booking`)
+      .then((data) => {
+        return data.json();
+      }).then((roomInfo) => {
+        const {nightly_price, past_week_views, cleaning_fee, service_fee, last_updated, review_count, avg_rating} = roomInfo[0];
+        this.setState({
+          nightlyPrice: nightly_price, 
+          pastWeekViews: past_week_views, 
+          cleaningFee: cleaning_fee, 
+          serviceFee: service_fee, 
+          lastUpdated: last_updated, 
+          reviewCount: review_count, 
+          avgRating: avg_rating
+        });
+      });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <div>
--- spec/App.test.jsx
+++ spec/App.test.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import App from '../client/components/App.jsx';
-
-configure({ adapter: new Adapter() });
 
 describe('App component', () => {
   it('should render correctly', () => {
-    const component = shallow(<App />);
-    expect(component).toMatchSnapshot();
+    const wrapper = shallow(<App />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
